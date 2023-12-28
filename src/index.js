a8b7b34658f31cb5d2680786c7fc8163f86a4315
Daniya Yelemessova <daniya@collingsmail.com>
2023-12-28 18:07:26 -0500

    Add trend API

--- src/index.js
+++ src/index.js
@@ -38,6 +38,38 @@ function handleForm(e){
   getGifs(search);
 }
 
+function showTrends(e){
+  e.preventDefault();
+  const trend = new XMLHttpRequest();
+  const url = `https://api.giphy.com/v1/gifs/trending?api_key=jPrtAVlEkgB3iUXBXWox8FSqSVBWO0i1&limit=10&offset=0&rating=g&bundle=messaging_non_clips`;
+  trend.addEventListener("loadend", function(){
+    const response = JSON.parse(this.responseText);
+    if(this.status === 200){
+      printTrends(response);
+    }
+  });
+  trend.open("GET", url, true);
+  trend.send();
+}
+
+function printTrends(apiResponse){
+  const outputDiv = document.querySelector('#trendOutput');
+  outputDiv.innerHTML = ''; 
+
+  apiResponse.data.forEach(gif => {
+    const imageUrl = gif.images.original.url;
+
+    const imgElement = document.createElement('img');
+    imgElement.src = imageUrl;
+    imgElement.alt = 'Trending GIF';
+    outputDiv.appendChild(imgElement);
+
+
+  });
+}
+  
+
 document.getElementById("form").addEventListener("submit", handleForm);
+document.getElementById("trends").addEventListener("click", showTrends);
 
 
